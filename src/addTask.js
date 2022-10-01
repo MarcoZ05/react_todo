faa446e26fc1a32bb26c34c11ea5bdddd086485a
Käkäsh <77887301+MarcoZ05@users.noreply.github.com>
2022-10-01 18:37:06 +0200

    delete input.value after adding task

--- src/addTask.js
+++ src/addTask.js
@@ -3,7 +3,9 @@ import { reloadTasks } from "./index.js";
 export default function addTask() {
   const newTaskName = document.getElementById("taskInput").value;
   const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
   if (newTaskName !== "" && !tasks.some((task) => task.name === newTaskName)) {
+    document.getElementById("taskInput").value = "";
     tasks.push({ name: newTaskName, completed: false });
     localStorage.setItem("tasks", JSON.stringify(tasks));
     reloadTasks();
